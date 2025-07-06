bcf3b199425b2d8171affbceb09601a4d564987a
Alexander Barrantes Herrera <abarrantesh@yahoo.com>
2025-07-05 19:21:25 -0600

    Delete conftest.py

--- conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from pyspark.sql import SparkSession
-
-@pytest.fixture(scope="session")
-def spark_session():
-    return SparkSession.builder \
-        .master("local[2]") \
-        .appName("pytest-spark") \
-        .getOrCreate()
